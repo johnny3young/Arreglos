69c8ddf4fd7eb5d45f2684ca61ad0ddc142ab09c
Black3 <youngerblack@gmail.com>
2019-08-03 22:32:08 -0500

    Update project with message in english

--- app/src/main/java/com/example/arreglos/MainActivity.kt
+++ app/src/main/java/com/example/arreglos/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    var totalNombres : Int = 0
+    var totalNames : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         val hermanachos = mutableListOf("Johnny I", "Johnny II", "Johnny III", "Johnny IV", "Stefanny")
 
         println("*******************************************************************************")
+        println("Showing data of the arrays:")
         println(hermanachos.get(0))
         println(hermanachos.get(1))
         println(hermanachos.get(2))
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         println(hermanachos.get(4))
         println("*******************************************************************************")
 
-        //Print all data of the array with cycle For
+        println("Print all data of the array with cycle For:")
         for (storage in hermanachos){
             println(storage)
         }
@@ -37,32 +38,32 @@ class MainActivity : AppCompatActivity() {
         hermanachos.set(0,"The lawyer of the family")
         hermanachos.set(3, "The younger brother")
 
-        //Showing the new data in array
+        println("Showing the data updates in array:")
         println(hermanachos.get(0))
         println(hermanachos.get(3))
         println("*******************************************************************************")
 
-        //Printing all the array with the new in data
-        for (posicion in hermanachos.indices) {
-            println(hermanachos.get(posicion))
+        println("Printing all the array with the new in data:")
+        for (storage in hermanachos.indices) {
+            println(hermanachos.get(storage))
         }
         println("*******************************************************************************")
 
-        //Printing each position and value of the array
-        for ((position, valor) in hermanachos.withIndex()){
-            println("In the position $position the value found is $valor")
+        println("Printing each position and value of the array:")
+        for ((position, value) in hermanachos.withIndex()){
+            println("In the index or position $position the value found is $value")
         }
         println("*******************************************************************************")
 
-        hermanachos.size //Muestra el tamaño de la lista
-        hermanachos.first() //Devuelve el primer valor
-        hermanachos.last() //Devuelve el último valor
-        hermanachos.get(2) //Devuelve el valor de la posición 2
+        hermanachos.size //Show size of the list
+        hermanachos.first() //Return first value
+        hermanachos.last() //Return last value
+        hermanachos.get(2) //Return value of the position 2
 
         println(hermanachos) //["Johnny I", "Johnny II", "Johnny III", "Johnny IV", "Stefanny"]
         println("*******************************************************************************")
 
-        //Adding or update an element (By default add in the end)
+        println("Adding or update an element (By default add in the end):")
         hermanachos.add("Daysi")
         hermanachos.add(0,"Naruto")
         hermanachos.add(7,"Luffy")
@@ -73,33 +74,34 @@ class MainActivity : AppCompatActivity() {
         hermanachos.none() //Nos devuelve un true si está vacía la lista
         hermanachos.firstOrNull() //Nos devolverá el primer campo, y si no hay, un null.
         hermanachos.elementAtOrNull(2) //El elemento del índice 2, si no hay, devolverá un null
-        hermanachos.lastOrNull() //Último valor de la lista o null
+        hermanachos.lastOrNull() //Último value de la lista o null
 
-        //El .filter nos permite filtrar en la lista a través de una o varias condiciones que pongamos.
-        // Para ello llamamos a it (iterator) y buscaremos en la lista, si contiene la palabra “Yohnny” o “Stefanny“. En este caso solo pintará “Stefanny“.
-        val filtro = hermanachos.filter { it == "Yohnny" || it == "Stefanny"}
-        println(filtro)
+        //The .filter our let filter in the list though of one o many conditions that we set. We call a it (iterator)
+        println("We find out in the list if contents the word “Yohnny” o “Stefanny“. In this case draws “Stefanny")
+        val filter = hermanachos.filter { it == "Yohnny" || it == "Stefanny"}
+        println(filter)
         println("*******************************************************************************")
 
-        //Removing an element
+        println("Showing data of the array without element Daysi removed:")
         hermanachos.remove("Daysi")
         println(hermanachos)
         println("*******************************************************************************")
 
         for ((index, item) in hermanachos.withIndex()){
-            println("En el indice $index se encuentra el nombre $item")
-            totalNombres = hermanachos.size
+            println("In the index $index the value found is $item")
+            totalNames = hermanachos.size
 
         }
-        println("En total hay $totalNombres nombres o registros")
+        println("Ther is $totalNames names o registries in total")
         println("*******************************************************************************")
 
         //Copy an array and can modify
-        val nuevaListaHermanos = mutableListOf<String>()
+        val newListBrothers = mutableListOf<String>()
         hermanachos.forEach {
-            nuevaListaHermanos.add("$it:")
+            newListBrothers.add("$it:")
         }
-        println(nuevaListaHermanos)
+        println("The new list modified is:")
+        println(newListBrothers)
         println("*******************************************************************************")
     }
 }
